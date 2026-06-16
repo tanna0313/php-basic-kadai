c5997737b91ce8e892b6a5b66a76fa78bd3800ff
tanna0313 <tanna0313@gmail.com>
2026-06-17 04:22:55 +0900

    Add files via upload

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>課題007</title>
+</head>
+<body>
+
+<?php
+
+$item = [
+    'name' => 'onion',
+    'price' => 200,
+    'weight' => 160
+];
+
+print_r($item);
+
+?>
+
+</body>
+</html>
